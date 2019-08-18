3005b7053897d6eade211536c02b1c4bb948df25
BrokenEarthDev <37110255+BrokenEarth@users.noreply.github.com>
2019-08-18 21:33:58 +0300

    Added javadocs

--- src/main/java/me/brokenearthdev/simpleyaml/entities/YamlElement.java
+++ src/main/java/me/brokenearthdev/simpleyaml/entities/YamlElement.java
@@ -1,6 +1,22 @@
+/*
+Copyright 2019 BrokenEarthDev
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package me.brokenearthdev.simpleyaml.entities;
 
 import org.jetbrains.annotations.NotNull;
+import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
 
 import java.util.HashMap;
@@ -26,40 +42,85 @@ public class YamlElement {
      */
     private Object value;
 
-   
-    private String asString;
+    /**
+     * Key-value pairs written in yaml syntax
+     */
+    private String yaml;
+
+    /**
+     * Key-value pairs stored in a map
+     */
     private Map<Object, Object> map;
 
-    public YamlElement(@NotNull String key, @NotNull Object value) {
-        Objects.requireNonNull(key);
-        Objects.requireNonNull(value);
-        Yaml yaml = new Yaml();
+    /**
+     * This constructor requires a key-value pair to be passed into its parameters where
+     * the key is the highest in the hierarchy containing a value. The value may contain
+     * nodes.
+     * <p>
+     * Using separators to represent a decent in hierarchy will not work. Instead, pass in
+     * a map as the value.
+     *
+     * @param key The key that contains a value
+     * @param value The value that its key contains
+     * @param options Contains convenient options that adjust styles in yaml to meet your
+     *                preferences
+     */
+    public YamlElement(@NotNull String key, @NotNull Object value, DumperOptions options) {
+        Yaml yaml = (options != null) ? new Yaml(options) : new Yaml();
         this.key = key;
         this.value = value;
         this.map = new HashMap<>();
         this.map.put(key, value);
-        this.asString = yaml.dump(map);
+        this.yaml = yaml.dump(map);
     }
 
+    /**
+     * This constructor requires a key-value pair to be passed into its parameters where
+     * the key is the highest in the hierarchy containing a value. The value may contain
+     * nodes.
+     * <p>
+     * Using separators to represent a decent in hierarchy will not work. Instead, pass in
+     * a map as the value.
+     *
+     * @param key The key that contains a value
+     * @param value The value that its key contains
+     */
+    public YamlElement(@NotNull String key, @NotNull Object value) {
+        this(key, value, null);
+    }
+
+    /**
+     * A key contains a value, retrieved by calling {@link #getValue()}
+     *
+     * @return The key that contains a value.
+     */
     public String getKey() {
         return key;
     }
 
+    /**
+     * A value is the data that its key has, retrieved by calling
+     * {@link #getKey()}
+     *
+     * @return The value that its key contains
+     */
     public Object getValue() {
         return value;
     }
 
+    /**
+     * This method returns a map containing the key-value pair passed into the
+     * constructor.
+     *
+     * @return The key-value pair in a map
+     */
     public Map<?, ?> getAsMap() {
         return map;
     }
 
-    public String getAsString() {
-        return asString;
-    }
-
     @Override
     public String toString() {
-        return asString;
+        return yaml;
     }
 
 }
